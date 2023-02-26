bac403d4c001b3a948fb5cf4188387d1b511d11e
ramnathbishwas <rambis075@gmail.com>
2023-02-26 22:49:14 +0530

    Add files via upload

---
+++ MyContactListController.js
@@ -0,0 +1,24 @@
+({
+    doInit : function(component) {
+        component.set("v.Columns", [
+            {label:"First Name", fieldName:"FirstName", type:"text"},
+            {label:"Last Name", fieldName:"LastName", type:"text"},
+            {label:"Phone", fieldName:"Phone", type:"phone"}
+        ]);
+        
+        var action = component.get("c.getContacts");
+        action.setParams({
+            recordId: component.get("v.recordId")
+           
+        });
+      
+
+        action.setCallback(this, function(data) {
+            component.set("v.Contacts", data.getReturnValue());
+           
+        
+
+    });
+    $A.enqueueAction(action); 
+}  
+})
